0d671e9622072b36d1906ecfc73d8edc194663ed
thanhbk113 <thanhbk11a1@gmail.com>
2022-11-28 11:57:49 +0700

    disable corrs

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	configs "my-kanban/config"
-	"my-kanban/middleware"
 	"my-kanban/routes"
 
 	"github.com/gin-gonic/gin"
@@ -12,14 +11,14 @@ func main() {
 
 	router := gin.New()
 	Port := configs.GetEnvName("PORT")
+	//router.Use(middleware.CORSMiddleware()) //use middleware cors for all route
 
+	router.Use(gin.Logger()) //loggin request
+
+	routes.KanbanRoutes(router) //use routes
 	if Port == "" {
 		Port = "8080"
 	}
-
-	router.Use(gin.Logger())                //loggin request
-	router.Use(middleware.CORSMiddleware()) //use middleware cors for all route
-	routes.KanbanRoutes(router)             //use routes
 
 	router.Run(":" + Port)
 
--- middleware/corsMiddleware.go
+++ middleware/corsMiddleware.go
@@ -1,12 +1,15 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "*")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
 
 		if c.Request.Method == "OPTIONS" {
